2d0157205a2a161af85531963ccbe5c499c4acc7
Ghara <anggarairham89@gmail.com>
2023-10-30 18:54:24 +0700

    Menambah code awal kasus liskov

---
+++ lsp/Product.java
@@ -0,0 +1,10 @@
+package lsp;
+import java.sql.Date; 
+abstract class Product {
+    abstract String getName(); 
+    abstract Date getExpiredDate();
+
+    public void getProductInfo(){
+    }
+
+}
---
+++ lsp/Smartphone.java
@@ -0,0 +1,13 @@
+package lsp;
+import java.sql.Date;
+public class Smartphone extends Product{
+    @Override
+    String getName(){
+    return "Samsung S22 Ultra";
+    }
+    @Override
+    Date getExpiredDate(){ 
+        return new Date(0);
+    }
+
+}
