17a66cf2ed66a9e942c0e21cb076cfdd418179af
Ghara <anggarairham89@gmail.com>
2023-10-30 19:21:40 +0700

    create isp

---
+++ isp/Motorcycle.java
@@ -0,0 +1,24 @@
+package isp;
+
+public class Motorcycle implements VehicleInterface{
+    // Can be implemented @Override
+public void drive() {
+}
+
+
+@Override
+public void stop() {
+}
+
+
+@Override
+public void refuel() {
+}
+
+
+// Can not be implemented karena motor tidak punya pintu 
+@Override
+public void openDoors() {
+}
+
+}
